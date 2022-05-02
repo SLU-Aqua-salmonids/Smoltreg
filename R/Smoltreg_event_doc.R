a59677c7d508c0a8996d6ead376eb08b1b3591f5
Anders Kagervall <Anders.Kagervall@slu.se>
2022-05-02 13:58:33 +0200

    Update documentation for Smoltreg_events data.frame

--- R/Smoltreg_event_doc.R
+++ R/Smoltreg_event_doc.R
@@ -2,10 +2,13 @@
 #' 
 #' Constants for all allowed numbers in column "HÄNDELSE" in the Smoltreg format
 #' 
-#' @format A data frame with 386469 rows and 29 variables:
+#' @format Smoltreg_events data.frame
 #' \describe{
-#'   \item{serial}{serial number, numeric}
-#'   \item{tag_number}{carlin tag number, character}
+#'   \item{UNKNOWN}{0, unknown event type}
+#'   \item{CAUGHT}{1, the fish was caught but not released to be part of recapture experiment}
+#'   \item{MARKED}{2, the fish was marked and released uppstreams, can be recaught}
+#'   \item{RECAPTURED}{3, the fish was recaptured}
+#'   \item{REMOVED}{4, the fish was removed, i.e. dead in trap, taken as sample...}
 #' }
 #'
 "Smoltreg_event"
